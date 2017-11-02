02773ebd9efe87a66f2daf76c4b8ea0478b91362
Sergey <sunliveua@gmail.com>
2017-11-02 14:01:31 +0200

    init

---
+++ src/main.js
@@ -0,0 +1,40 @@
+
+import Vue from 'vue'
+
+import {
+  Vuetify,
+  VApp,
+  VNavigationDrawer,
+  VFooter,
+  VList,
+  VBtn,
+  VIcon,
+  VGrid,
+  VToolbar,
+  transitions
+} from 'vuetify'
+import './stylus/main.styl'
+
+import App from './App'
+
+Vue.use(Vuetify, {
+  components: {
+    VApp,
+    VNavigationDrawer,
+    VFooter,
+    VList,
+    VBtn,
+    VIcon,
+    VGrid,
+    VToolbar,
+    transitions
+  }
+})
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  render: h => h(App)
+})
