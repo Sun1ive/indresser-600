21ed3f93fc3754607b32e772435962444e409f6a
Sergey <sunliveua@gmail.com>
2017-11-06 22:34:38 +0200

    0.3.4.9

--- src/components/category/coats.vue
+++ src/components/category/coats.vue
@@ -10,10 +10,15 @@
           </div>
         </v-flex>
         <v-flex xs12 sm12 md6 lg6>
-          <v-carousel hide-controls>
+<!--           <v-carousel hide-controls>
             <v-carousel-item v-for="(item, i) in coats" :key="i" :src="item"></v-carousel-item>
             <div class="myButton" @click.stop="drawer = true">Купить</div>
-          </v-carousel>
+          </v-carousel> -->
+          <slider  width="100%" :interval="6000" animation="fade" :speed="900" height="100%">
+            <slider-item v-for="(item, i) in coats" :key="i">
+              <img :src="item" alt="coat">
+            </slider-item>
+          </slider>
         </v-flex>
       </v-layout>
     </v-container>
@@ -25,9 +30,13 @@
 
 <script>
 import order from '../modals/orderCoat'
+import { Slider, SliderItem } from 'vue-easy-slider'
+
 export default {
   components: {
-    'app-order': order
+    'app-order': order,
+    Slider,
+    SliderItem
   },
   data() {
     return {
