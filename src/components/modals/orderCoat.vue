700892554a9fdbb12535a42f5bdba4bc28c82818
Sergey <sunliveua@gmail.com>
2017-11-09 15:30:57 +0200

    0.4.2.2

--- src/components/modals/orderCoat.vue
+++ src/components/modals/orderCoat.vue
@@ -1,13 +1,8 @@
 <template>
   <v-container fluid>
     <v-layout>
-      <!-- <v-flex>
-        <img src="/static/img/long.jpg" alt="coats">
-        <img src="../../assets/img/order/long.jpg" alt="coats"> 
-      </v-flex> -->
       <v-form class="orderForm" @submit.prevent="submit">
         <h2>Купить пальто длинное</h2>
-
         <v-layout class="formWrapper">
           <v-flex class="mr-3">
             <v-select :items="colors" v-model="userData.color" label="Цвет"></v-select>
@@ -16,14 +11,9 @@
             <v-select :items="sizes" v-model="userData.size" label="Размер"></v-select>
           </v-flex>
         </v-layout>
-
-
-
-
-        <v-text-field required v-model="userData.name" label="Ваше имя и фамилия"></v-text-field>
-        <v-text-field required v-model="userData.adress" label="Адресс доставки"></v-text-field>
+        <v-text-field required v-model="userData.fullName" label="Ваше имя и фамилия"></v-text-field>
+        <v-text-field required v-model="userData.address" label="Адресс доставки"></v-text-field>
         <v-select :items="payment" v-model="userData.payment" label="Способ оплаты"></v-select>
-        <!-- <v-text-field required v-model="userData.email" label="Введите Ваш e-mail"></v-text-field> -->
         <v-text-field required v-model="userData.phone" label="Ваш телефон"></v-text-field>
         <button class="myButton">Подтвердить заказ</button>
         <div class="close" @click="close"></div>
@@ -45,14 +35,14 @@ export default {
   },
   data() {
     return {
-      drawer: true,
+      drawer: false,
       userData: {
         fullName: '',
         phone: '',
         email: '',
         size: null,
         color: null,
-        adress: '',
+        address: '',
         payment: '',
       },
       sizes: ['XS', 'S', 'M', 'XL'],
@@ -66,28 +56,36 @@ export default {
       if (validate.test(this.userData.phone)) {
       Email.send(
         `coats@indresser.com`,
-        'info@indresser.com',
+        // 'info@indresser.com',
+        'sunliveua@gmail.com',
         'Заказ пальто с сайта landing.indresser.com',
         `Пользователь: ${this.userData.fullName},
+        Заказал: пальто длинное,
+        Размер: ${this.userData.size},
+        Цвет: ${this.userData.color},
+        Адресс доставки: ${this.userData.address},
+        Способ оплаты: ${this.userData.payment},
         Телефон: ${this.userData.phone}`,
         'mail.adm.tools',
         'coats@indresser.com',
         '3DLao3x1AC8t'
       );
 
-      this.$http
-        .post('https://myvuewebapp.firebaseio.com/order.json', this.userData)
-        .then(r => console.log(r))
-        .catch(e => console.log(e));
+      // this.$http
+      //   .post('https://myvuewebapp.firebaseio.com/order.json', this.userData)
+      //   .then(r => console.log(r))
+      //   .catch(e => console.log(e));
 
       this.userData = {
         fullName: '',
         phone: '',
         email: '',
         size: null,
-        adress: '',
+        address: '',
         payment: ''
       };
+      
+      this.drawer = true
 
       this.$emit('closeForm');
       } else {
@@ -105,6 +103,19 @@ export default {
 <style scoped lang="stylus">
 .orderForm
   background-color #fff
-  padding 0 1rem
+  padding 1rem
+  position relative
+  width 100%
+  height 100%
+  h2
+    font-size responsive 1rem 2rem
+    text-align center
+  .myButton
+    height 50px
+    width 200px
+    padding-left 1rem
+  .close
+    top 2%
+    right 2%
 
 </style>
